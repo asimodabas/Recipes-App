264d8fd0f05667dd187b1cf3eb3284745ce63a2d
WindowsS <Asimodabas03@hotmail.com>
2023-02-27 19:35:39 +0300

    packaging done, api base url added to constants

--- app/src/main/java/com/asimodabas/recipes_app/ui/fragment/foodjoke/FoodJokeFragment.kt
+++ app/src/main/java/com/asimodabas/recipes_app/ui/fragment/foodjoke/FoodJokeFragment.kt
@@ -1,11 +1,11 @@
-package com.asimodabas.recipes_app.ui.fragment
+package com.asimodabas.recipes_app.ui.fragment.foodjoke
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.asimodabas.recipes_app.R
 import com.asimodabas.recipes_app.databinding.FragmentFoodJokeBinding
-import com.asimodabas.recipes_app.viewBinding
+import com.asimodabas.recipes_app.util.viewBinding
 
 class FoodJokeFragment : Fragment(R.layout.fragment_food_joke) {
 
