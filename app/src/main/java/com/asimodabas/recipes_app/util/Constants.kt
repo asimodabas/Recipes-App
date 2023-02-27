116c66da6073eccc7d77564c30335da2be6e0e4d
WindowsS <Asimodabas03@hotmail.com>
2023-02-27 19:41:39 +0300

    getRecipes fun @GET for retrofit

--- app/src/main/java/com/asimodabas/recipes_app/util/Constants.kt
+++ app/src/main/java/com/asimodabas/recipes_app/util/Constants.kt
@@ -3,6 +3,6 @@ package com.asimodabas.recipes_app.util
 object Constants {
 
     const val BASE_URL = "https://api.spoonacular.com"
-    const val API_KEY = ""
+    const val API_KEY = "fcf86100d7814552a2aed7c74e8a523d"
 
 }
