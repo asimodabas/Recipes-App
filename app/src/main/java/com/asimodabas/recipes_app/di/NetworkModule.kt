a80bc5acb05c61576a5c60b5090a16b02437997a
WindowsS <Asimodabas03@hotmail.com>
2023-03-02 18:56:48 +0300

    NetworkModule added @Module @InstallIn @Singleton @Provides

---
+++ app/src/main/java/com/asimodabas/recipes_app/di/NetworkModule.kt
@@ -0,0 +1,46 @@
+package com.asimodabas.recipes_app.di
+
+import com.asimodabas.recipes_app.network.FoodRecipesApi
+import com.asimodabas.recipes_app.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient, gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder().baseUrl(BASE_URL).client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): FoodRecipesApi {
+        return retrofit.create(FoodRecipesApi::class.java)
+    }
+}
