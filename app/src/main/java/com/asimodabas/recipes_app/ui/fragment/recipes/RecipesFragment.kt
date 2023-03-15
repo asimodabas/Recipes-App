52c1f5ecdd56e75d22fc3eb922ab15500e053ec8
WindowsS <Asimodabas03@hotmail.com>
2023-03-15 21:46:41 +0300

    setupRecyclerView

--- app/src/main/java/com/asimodabas/recipes_app/ui/fragment/recipes/RecipesFragment.kt
+++ app/src/main/java/com/asimodabas/recipes_app/ui/fragment/recipes/RecipesFragment.kt
@@ -3,15 +3,36 @@ package com.asimodabas.recipes_app.ui.fragment.recipes
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.asimodabas.recipes_app.R
+import com.asimodabas.recipes_app.adapter.RecipesAdapter
 import com.asimodabas.recipes_app.databinding.FragmentRecipesBinding
 import com.asimodabas.recipes_app.util.viewBinding
 
 class RecipesFragment : Fragment(R.layout.fragment_recipes) {
 
     private val binding by viewBinding(FragmentRecipesBinding::bind)
+    private val mAdapter by lazy { RecipesAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerview.adapter = mAdapter
+        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun showShimmerEffect() {
+        binding.shimmerFrameLayout.startShimmer()
+        binding.shimmerFrameLayout.visibility = View.VISIBLE
+        binding.recyclerview.visibility = View.GONE
+    }
+
+    private fun hideShimmerEffect() {
+        binding.shimmerFrameLayout.stopShimmer()
+        binding.shimmerFrameLayout.visibility = View.GONE
+        binding.recyclerview.visibility = View.VISIBLE
     }
 }
